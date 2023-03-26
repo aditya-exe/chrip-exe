470068c430a0150eb5f558472ab62813f8537ec3
aditya <pokemaniacaditya@gmail.com>
2023-03-26 15:53:01 +0530

    ADD:ratelimiter

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -4,10 +4,18 @@ import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
 import { clerkClient } from "@clerk/nextjs/server";
 import type { User } from "@clerk/nextjs/dist/api";
 import { TRPCError } from "@trpc/server";
+import { Redis } from '@upstash/redis';
+import { Ratelimit } from '@upstash/ratelimit';
 
 function filterUserForClient(user: User) {
   return { id: user.id, userName: user.username, profileImage: user.profileImageUrl };
 }
+
+const rateLimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(3, "1 m"),
+  analytics: true,
+});
 
 export const postsRouter = createTRPCRouter({
   getAll: publicProcedure.query(async ({ ctx }) => {
@@ -45,6 +53,12 @@ export const postsRouter = createTRPCRouter({
     content: z.string().emoji().min(1).max(280),
   })).mutation(async ({ ctx, input }) => {
     const authorId = ctx.userId;
+
+    const { success } = await rateLimit.limit(authorId);
+
+    if (!success) {
+      throw new TRPCError({ code: "TOO_MANY_REQUESTS" });
+    }
 
     const post = await ctx.prisma.post.create({
       data: {
