5bdc669b3eea9fe3f4c49396d8e824c20238c2b3
aditya <pokemaniacaditya@gmail.com>
2023-03-28 20:11:10 +0530

    FIX:routing in post view

--- src/components/post-view/index.tsx
+++ src/components/post-view/index.tsx
@@ -4,6 +4,7 @@ import relativeTime from "dayjs/plugin/relativeTime";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { type MouseEvent } from "react";
 
 dayjs.extend(relativeTime);
 
@@ -11,30 +12,45 @@ const PostView = (props: PostWithUser) => {
   const { post, author } = props;
   const router = useRouter();
 
+  function handleRouteToPost(
+    e: MouseEvent<HTMLDivElement>,
+    postId: string
+  ): void {
+    // e.preventDefault();
+    e.stopPropagation();
+
+    router.push(`/post/${postId}`).catch((err) => console.error(err));
+  }
+
   return (
     <div
       key={post.id}
-      className={"flex items-center gap-x-4 border-b border-slate-400 p-8"}
+      className={
+        "flex cursor-pointer items-center gap-x-4 border-b border-slate-400 p-8"
+      }
+      onClick={(e) => handleRouteToPost(e, post.id)}
     >
       <Image
         src={author.profileImage}
         alt={`@${author.userName}'s Profile Picture`}
-        className={"h-14 rounded-full cursor-pointer"}
+        className={
+          "h-14 cursor-pointer rounded-full transition-all hover:ring-2 hover:ring-slate-400"
+        }
         height={56}
         width={56}
         onClick={(e) => {
-          e.preventDefault();
+          e.stopPropagation();
           router.push(`/${author.userName}`).catch((err) => console.error(err));
         }}
       />
       <div className="flex flex-col">
-        <div className="flex font-bold text-slate-400 ">
-          <Link href={`/@${author.userName}`}>
-            <span>{`@${author.userName} `}</span>
-          </Link>
-          <Link href={`/post/${post.id}`}>
+        <div className="flex gap-x-2">
+          <UsernameHolder userName={author.userName} />
+          <div className="font-bold text-slate-400">
+            {/* <Link href={`/post/${post.id}`}> */}
             <span>{` - ${dayjs(post.createdAt).fromNow()}`}</span>
-          </Link>
+            {/* </Link> */}
+          </div>
         </div>
         <span className="text-2xl">{post.content}</span>
       </div>
@@ -43,3 +59,25 @@ const PostView = (props: PostWithUser) => {
 };
 
 export default PostView;
+
+const UsernameHolder = ({ userName }: { userName: string }) => {
+  // document.getElementById("username-link")?.addEventListener("click", (e) => {
+  //   e.stopPropagation();
+  // });
+
+  return (
+    <div
+      className="flex cursor-pointer font-bold text-slate-400"
+      id="username-link"
+    >
+      <Link
+        href={`/@${userName}`}
+        onClick={(e) => {
+          e.stopPropagation();
+        }}
+      >
+        <span>{`@${userName} `}</span>
+      </Link>
+    </div>
+  );
+};
